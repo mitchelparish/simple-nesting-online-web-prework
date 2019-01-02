a18bb6a7ef7b584a116bd5f23332a5f32a612d97
Mitchel Parish <mitchelparish@gmail.com>
2019-01-02 20:35:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,8 +91,10 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {}
-
+programmer_hash[:yukihiro_matsumoto] = {
+	:known_for => "Ruby",
+	:languages => ["LISP", "C"]
+	}
 
 end
 
